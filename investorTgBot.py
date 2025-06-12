e6b5a087a600ec65017d9ac7fb0f3a6387f180aa
Elad Winner <eladwinn87@gmail.com>
2025-06-12 20:24:01 +0300

    another bit better

--- investorTgBot.py
+++ investorTgBot.py
@@ -29,10 +29,10 @@ def investLogic(salary):
 def bot_session():
     TELEGRAM_API_URL = "https://api.telegram.org/bot"
     TOKEN = "7912633230:AAGtNjdItB-kg6XoaZh0GDIxC2lUVjHJFf8"
-    CHAT_ID = "380382224"
+    MY_CHAT_ID = "380382224"
     LOGS_PATH = "./logs"
 
-    requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": CHAT_ID, "text":
+    requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text":
         "Hey, it's time for investments📈💵!"
         "\nwhat's the salary this month?💰"
         "\nI'll be here for the next hour :) "})
@@ -43,21 +43,21 @@ def bot_session():
 
     while i < 3600 and response_timestamp < session_timestamp:
         user_input = requests.get(f"{TELEGRAM_API_URL}{TOKEN}/getUpdates?offset=-1").json()
-        last_chat_id = user_input['result'][0]['message']['chat']['id']
+        chat_id = user_input['result'][0]['message']['chat']['id']
         response_timestamp = user_input.get("result", [{}])[0].get("message", {}).get("date", "")
 
         if i == 2700:
-            requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": CHAT_ID, "text": "Turning off in 15 minutes⏳"})
-            
+            requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text": "Turning off in 15 minutes⏳"})
+
         i += 1
 
-    if response_timestamp < session_timestamp and last_chat_id == CHAT_ID:
-        requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": CHAT_ID, "text": "Okay see you next month I guess!"})
+    if response_timestamp < session_timestamp and chat_id == MY_CHAT_ID:
+        requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text": "Okay see you next month I guess!"})
     else:
         salary = user_input.get("result", [{}])[0].get("message", {}).get("text", "")
 
         result = investLogic(float(salary))
-        requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": CHAT_ID, "text": f"Calculations for {salary} ILS:\n{result}"})
+        requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text": f"Calculations for {salary} ILS:\n{result}"})
         with open(f"{LOGS_PATH}/invest_guide_{datetime.datetime.now().strftime("%d-%m-%Y")}.txt", "w") as f:
            print(result, file=f)
 
