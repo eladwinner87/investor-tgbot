44b4c525c0739ea1495d75ae2b12776c582afa42
Elad Winner <eladwinn87@gmail.com>
2025-06-13 15:29:30 +0300

    some upgrades and docker-compose

--- investorTgBot.py
+++ investorTgBot.py
@@ -9,12 +9,13 @@ def getBlinkRate():
     API_URL = os.getenv("API_URL")
     API_KEY = os.getenv("API_KEY")
 
-    # response = requests.get(API_URL, params={
-    #     "access_key": API_KEY,
-    #     "symbols": "USD,ILS",
-    # }).json()
+    response = requests.get(API_URL, params={
+        "access_key": API_KEY,
+        "symbols": "USD,ILS",
+    }).json()
 
-    response = {'success': True, 'timestamp': 1748966344, 'base': 'EUR', 'date': '2025-06-03', 'rates': {'USD': 1.137132, 'ILS': 4.002764}}
+    # EXAMPLE RESPONSE:
+    # response = {'success': True, 'timestamp': 1748966344, 'base': 'EUR', 'date': '2025-06-03', 'rates': {'USD': 1.137132, 'ILS': 4.002764}}
 
     realRate = response['rates']['ILS'] / response['rates']['USD']
     return realRate * 1.014204545454545
@@ -32,11 +33,11 @@ def investLogic(salary):
 
 def bot_session():
     TELEGRAM_API_URL = "https://api.telegram.org/bot"
-    TOKEN = os.getenv("TOKEN")
-    MY_CHAT_ID = os.getenv("MY_CHAT_ID")
-    LOGS_PATH = os.getenv("LOGS_PATH", "./logs")
+    token = os.getenv("TG_BOT_TOKEN")
+    myChatId = os.getenv("MY_CHAT_ID")
+    logsPath = os.getenv("LOGS_PATH", "./logs")
 
-    requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text":
+    requests.get(f"{TELEGRAM_API_URL}{token}/sendMessage", data={"chat_id": myChatId, "text":
         "Hey, it's time for investments📈💵!"
         "\nwhat's the salary this month?💰"
         "\nI'll be here for the next hour :) "})
@@ -46,23 +47,23 @@ def bot_session():
     response_timestamp, i = 0, 0
 
     while i < 3600 and response_timestamp < session_timestamp:
-        user_input = requests.get(f"{TELEGRAM_API_URL}{TOKEN}/getUpdates?offset=-1").json()
+        user_input = requests.get(f"{TELEGRAM_API_URL}{token}/getUpdates?offset=-1").json()
         chat_id = user_input['result'][0]['message']['chat']['id']
         response_timestamp = user_input.get("result", [{}])[0].get("message", {}).get("date", "")
 
         if i == 2700:
-            requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text": "Turning off in 15 minutes⏳"})
+            requests.get(f"{TELEGRAM_API_URL}{token}/sendMessage", data={"chat_id": myChatId, "text": "Turning off in 15 minutes⏳"})
 
         i += 1
 
-    if response_timestamp < session_timestamp and chat_id == MY_CHAT_ID:
-        requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text": "Okay see you next month I guess!"})
+    if response_timestamp < session_timestamp and chat_id == myChatId:
+        requests.get(f"{TELEGRAM_API_URL}{token}/sendMessage", data={"chat_id": myChatId, "text": "Okay see you next month I guess!"})
     else:
         salary = user_input.get("result", [{}])[0].get("message", {}).get("text", "")
-
+        
         result = investLogic(float(salary))
-        requests.get(f"{TELEGRAM_API_URL}{TOKEN}/sendMessage", data={"chat_id": MY_CHAT_ID, "text": f"Calculations for {salary} ILS:\n{result}"})
-        with open(f"{LOGS_PATH}/invest_guide_{datetime.datetime.now().strftime("%d-%m-%Y")}.txt", "w") as f:
+        requests.get(f"{TELEGRAM_API_URL}{token}/sendMessage", data={"chat_id": myChatId, "text": f"Calculations for {salary} ILS:\n{result}"})
+        with open(f"{logsPath}/invest_guide_{datetime.datetime.now().strftime('%d-%m-%Y')}.txt", "w") as f:
            print(result, file=f)
 
 bot_session()
