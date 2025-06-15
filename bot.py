56a27629b1622bc23ca834a4daa636afef8580d2
Elad Winner <eladwinn87@gmail.com>
2025-06-15 12:05:08 +0300

    more

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-import requests
 import time, datetime
-from utils.telegramAPI import TelegramAPI
-from utils.investLogic import investLogic
 from config import Config
+from utils import telegramAPI, investLogic
+
+TelegramAPI = telegramAPI.TelegramAPI
+investLogic = investLogic.investLogic
 
 def bot():
-    token, logsPath = Config.BOT_TOKEN, Config.LOGS_PATH
-
     TelegramAPI().send_message("Hey, it's time for investments📈💵!\nwhat's the salary this month?💰\nTake your time :) ")
-
     session_timestamp, response_timestamp, i = int(time.time()), int(time.time()), 0
 
     while not response_timestamp > session_timestamp and i < Config.TIMEOUT:
@@ -17,14 +15,14 @@ def bot():
         response_timestamp = response.get("result", [{}])[0].get("message", {}).get("date", "")
 
         if i == Config.WARNING_TIME:
-            requests.get(f"{TELEGRAM_API_URL}{token}/sendMessage", data={"chat_id": chatId, "text": "Turning off soon⏳"})
+            TelegramAPI().send_message("Turning off soon⏳")
         i += 1
 
     if response_timestamp > session_timestamp and chat_id == Config.CHAT_ID:
         salary = int(response.get("result", [{}])[0].get("message", {}).get("text", ""))
         result = investLogic(salary)
         TelegramAPI().send_message(f"🧮 Calculations for {salary} ILS:\n{result}")
-        with open(f"{logsPath}/investor_sum_{datetime.datetime.now().strftime('%d-%m-%Y')}.txt", "w") as f:
+        with open(f"{Config.LOGS_PATH}/investor_sum_{datetime.datetime.now().strftime('%d-%m-%Y')}.txt", "w") as f:
            print(result, file=f)
     else:
         TelegramAPI().send_message("Okay see you next month I guess!")
--- utils/telegramAPI.py
+++ utils/telegramAPI.py
@@ -4,7 +4,7 @@ from config import Config
 class TelegramAPI:
     BASE_URL = f"https://api.telegram.org/bot{Config.BOT_TOKEN}"
     
-    def send_message(self, text: str):
+    def send_message(self, text):
         return requests.get(
             f"{self.BASE_URL}/sendMessage",
             data={"chat_id": Config.CHAT_ID, "text": text}
