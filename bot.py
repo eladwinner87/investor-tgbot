94ac3b476f9e39fd7f933f0ba28eb49ea45c051d
Elad Winner <eladwinn87@gmail.com>
2025-06-28 16:03:38 +0300

    moved retrieving logic bakwards

--- bot.py
+++ bot.py
@@ -1,30 +1,20 @@
-import time
 from config import Config
 from services import telegramAPI, investLogic
 
 TelegramAPI, investLogic = telegramAPI.TelegramAPI, investLogic.investLogic
 
 def bot():
-    SESSION_START = int(time.time())
     TelegramAPI.send_message("Hey, it's time for investments📈💵!\nwhat's the salary this month?💰\nTake your time :)")
+    
+    response = TelegramAPI.retrieve_response(Config.TIMEOUT, Config.WARNING_TIME)
 
-    while int(time.time()) - SESSION_START < Config.TIMEOUT:
-        TIME_ELAPSED = int(time.time()) - SESSION_START
-        response = TelegramAPI.retrieve_response()
-
-        if response["timestamp"] > SESSION_START:
-            break
-        elif int(time.time()) - SESSION_START == Config.WARNING_TIME:
-            TelegramAPI.send_message("Turning off soon⏳")
-            time.sleep(1)
-
-    if response["timestamp"] > SESSION_START:
+    if response["new"]:
         result = investLogic(response["salary"])
         TelegramAPI.send_message(result)
         
         with open(Config.LOG_FILENAME, "w") as f:
            print(result, file=f)
     else:
-        TelegramAPI().send_message("Okay see you next month I guess!")
+        TelegramAPI.send_message("Okay see you next month I guess!")
 
 bot()
--- services/telegramAPI.py
+++ services/telegramAPI.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from config import TelegramCreds
 
 class TelegramAPI:
@@ -13,13 +14,25 @@ class TelegramAPI:
             }
         )
     
-    def retrieve_response():
+    def retrieve_response(TIMEOUT, WARNING):
+        SESSION_START = int(time.time())
         base_response = requests.get(f"{TelegramAPI.BASE_URL}/getUpdates?offset=-1").json()["result"][0]["message"]
 
-        response = {}
+        while base_response["date"] <= SESSION_START and int(time.time()) - SESSION_START < TIMEOUT:
+            base_response = requests.get(f"{TelegramAPI.BASE_URL}/getUpdates?offset=-1").json()["result"][0]["message"]
 
+            if int(time.time()) - SESSION_START == WARNING:
+                TelegramAPI.send_message("Turning off soon⏳")
+                time.sleep(1)
+    
+        response = {}
         response["chat_id"] = int(base_response["chat"]["id"])
         response["timestamp"] = int(base_response["date"])
         response["salary"] = int(base_response["text"])
+
+        if base_response["date"] > SESSION_START:
+            response["new"] = True
+        else:
+            response["new"] = False
 
         return response
